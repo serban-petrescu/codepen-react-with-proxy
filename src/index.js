b110db14afc71e57b30183bef713ad9c7b79f2cb
Serban Petrescu <serban.petrescu@outlook.com>
2019-12-15 15:05:29 +0200

    Adds further dependencies.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./styles.css";
-
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
